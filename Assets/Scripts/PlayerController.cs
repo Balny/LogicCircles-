298a42963f537bfaf05522247ff5cd47ffdfbd11
Benjamin Guzman <guzmanb803@gmail.com>
2022-09-14 19:22:03 -0400

    made the player rotates depending on the current value of the node it hits

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,10 +18,22 @@ public class PlayerController : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.gameObject.name);
+        //Debug.Log(other.gameObject.name);
         DirectionalNodes dn = other.GetComponent<DirectionalNodes>();
         Debug.Log(dn.current);
-        transform.Rotate(Vector3.back * 90); //back rotates right and foward rotates left
+        float rot = 0;
+        Vector3 dir = Vector3.zero;
+        if (dn.current == "right")
+        {
+            dir = Vector3.back;
+            rot = 90;
+        }
+        else if (dn.current == "left")
+        {
+            dir = Vector3.forward;
+            rot = 90;
+        }
+        transform.Rotate(dir * rot); //back rotates right and foward rotates left
 
     }
 }
